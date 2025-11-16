98737cd0f7879b034010cedac0c4b7f48c5991ef
Isabely Lemos <isabelylemos24@gmail.com>
2025-11-16 12:50:07 -0300

    Initial commit

---
+++ src/main/java/br/com/faculdade/atividade_3_ci_cd/Atividade3CiCdApplication.java
@@ -0,0 +1,13 @@
+package br.com.faculdade.atividade_3_ci_cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atividade3CiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atividade3CiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/faculdade/atividade_3_ci_cd/Atividade3CiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.faculdade.atividade_3_ci_cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atividade3CiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
